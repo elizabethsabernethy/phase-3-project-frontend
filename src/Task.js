c246c63f8feb1af08296391965e4a3fcbad3be56
Elizabeth Abernethy <elizabethsabernethy@gmail.com>
2023-03-29 18:26:41 -0400

    adds important and urgent checkboxes, callback props for onchange of the checkboxes, and handler functions with patch requests for the important and urgent changes

--- src/Task.js
+++ src/Task.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-function Task({task, onDeleteTask}){
+function Task({task, onDeleteTask, onUpdateTaskImportance, onUpdateTaskUrgency}){
     const[completed, setCompleted]=useState(false)
 
     function handleDelete(){
@@ -10,6 +10,34 @@ function Task({task, onDeleteTask}){
             .then((resp) => resp.json())
             .then(() => onDeleteTask(task));
     }
+
+    function handleChangeOfImportance(){
+        fetch(`http://localhost:9292/tasks/importance/${task.id}`, {
+          method: "PATCH",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            important: !task.important,
+          }),
+        })
+          .then((resp) => resp.json())
+          .then((updatedTask) => onUpdateTaskImportance(updatedTask));
+        }
+    
+        function handleChangeOfUrgency(){
+        fetch(`http://localhost:9292/tasks/urgency/${task.id}`, {
+          method: "PATCH",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            urgent: !task.urgent,
+          }),
+        })
+          .then((resp) => resp.json())
+          .then((updatedTask) => onUpdateTaskUrgency(updatedTask));
+        }
 
     function handleEdit(){
         console.log('Edited')
@@ -21,6 +49,22 @@ function Task({task, onDeleteTask}){
     return(
         <div>
             <h4 style={completed ? {textDecorationLine: 'line-through', textDecorationStyle: 'solid'} : null}>{task.name}</h4>
+            <label>
+                Important
+                <input 
+                type="checkbox" 
+                checked={task.important}
+                onChange={handleChangeOfImportance}>
+                </input>
+            </label>
+            <label>
+                Urgent
+                <input 
+                type="checkbox"
+                checked={task.urgent}
+                onChange={handleChangeOfUrgency}>
+                </input>
+            </label>
             <button onClick={handleDelete}>🗑️</button>
             <button onClick={handleEdit}>✏️</button>
             <button onClick={handleComplete}>✅</button>
