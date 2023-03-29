25e239e3dc1616a5d3a47e4c82bcd429a637549e
Elizabeth Abernethy <elizabethsabernethy@gmail.com>
2023-03-29 17:08:21 -0400

    adds state for tasks to instantly update dom upon form submit, and changes list.tasks in map to new state of tasks

--- src/TaskList.js
+++ src/TaskList.js
@@ -3,9 +3,10 @@ import TaskForm from "./TaskForm";
 import Task from "./Task";
 
 function TaskList({list}){
+    const[tasks, setTasks] = useState(list.tasks)
 
     function handleNewTask(newTask){
-        console.log(newTask)
+       setTasks([...tasks, newTask])
     }
 
     function handleDelete(){
@@ -22,7 +23,7 @@ function TaskList({list}){
             <button onClick={handleDelete}>🗑️</button>
             <button onClick={handleEdit}>✏️</button>
             <TaskForm onAddNewTask={handleNewTask} list_id={list.id}/>
-            {typeof list.tasks !== "undefined" ? list.tasks.map((task)=>{
+            {typeof tasks !== "undefined" ? tasks.map((task)=>{
                 return <Task task={task} key={task.id}/>
             }): null} 
         </div>
