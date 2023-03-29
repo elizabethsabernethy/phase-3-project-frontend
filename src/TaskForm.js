2009845719869b8c162459b6f6f4416cdb425283
Elizabeth Abernethy <elizabethsabernethy@gmail.com>
2023-03-29 16:13:57 -0400

    uncomments post request, adds list_id props

--- src/TaskForm.js
+++ src/TaskForm.js
@@ -1,30 +1,30 @@
 import React, { useState } from "react";
 
-function TaskForm({onAddNewTask}){
+function TaskForm({onAddNewTask, list_id}){
 
     const[taskName, setTaskName]=useState("")
 
     function handleSubmit(e) {
       e.preventDefault();
-      // fetch("http://localhost:9292/tasks", {
-      //   method: "POST",
-      //   headers: {
-      //     "Content-Type": "application/json",
-      //   },
-      //   body: JSON.stringify({
-      //     name: taskName,
-      //     important: false,
-      //     urgent: false,
-      //     complete: false,
-      //     task_list_id: task_list_id <------- how do i get the correct id?
-      //   }
-      //   ),
-      // })
-      //   .then((resp) => resp.json())
-      //   .then((newTask) => {
-      //     onAddNewTask(newTask);
-      //     setTaskName("");
-      //   });
+      fetch("http://localhost:9292/tasks", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: taskName,
+          important: false,
+          urgent: false,
+          complete: false,
+          task_list_id: list_id
+        }
+        ),
+      })
+        .then((resp) => resp.json())
+        .then((newTask) => {
+          onAddNewTask(newTask);
+          setTaskName("");
+        });
     }
 
     function handleChange(e){
