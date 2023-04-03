3eb70601988958625c61b077ac397da85a7e4b27
Elizabeth Abernethy <elizabethsabernethy@gmail.com>
2023-04-03 11:30:02 -0400

    adds simple checkboxes with labels for later filtering

--- src/SortTaskLists.js
+++ src/SortTaskLists.js
@@ -3,7 +3,18 @@ import React from "react";
 function SortTaskLists(){
     return(
         <div>
-
+            <label>
+                Filter: Important
+                <input 
+                type="checkbox">
+                </input>
+            </label>
+            <label>
+                Filter: Urgent
+                <input 
+                type="checkbox">
+                </input>
+            </label>
         </div>
     )
 }
