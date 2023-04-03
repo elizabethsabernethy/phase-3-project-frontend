abdcc391a720b59e455b08919220d1ca9f1de350
Elizabeth Abernethy <elizabethsabernethy@gmail.com>
2023-04-03 11:38:33 -0400

    removes filter important and urgent checkboxes

--- src/SortTaskLists.js
+++ src/SortTaskLists.js
@@ -3,18 +3,7 @@ import React from "react";
 function SortTaskLists(){
     return(
         <div>
-            <label>
-                Filter: Important
-                <input 
-                type="checkbox">
-                </input>
-            </label>
-            <label>
-                Filter: Urgent
-                <input 
-                type="checkbox">
-                </input>
-            </label>
+            
         </div>
     )
 }
