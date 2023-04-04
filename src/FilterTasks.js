169f234f66d1a1a4d03da9d5ac83664d69d8c7af
Elizabeth Abernethy <elizabethsabernethy@gmail.com>
2023-04-04 10:11:37 -0400

    adds state, props and functions to handle filtering completed tasks

--- src/FilterTasks.js
+++ src/FilterTasks.js
@@ -1,10 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
-function FilterTasks(){
+function FilterTasks({tasks, setTasksToShow, tasksToShow}){
+    const[showCompleted, setShowCompleted] = useState(true)
+
+    const completedTasksToDisplay = tasks.filter((task) => {
+        if (task.completed) {
+        return task;
+        }
+        return false;
+      })
+
+    function handleCompletedFilter(){
+        setTasksToShow(completedTasksToDisplay);
+        setShowCompleted((showCompleted)=>!showCompleted)    
+    }
+
+    function handleShowAllTasks(){
+        setTasksToShow(tasks);
+        setShowCompleted((showCompleted)=>!showCompleted)
+    }
+
+    function toggleCompletedTasks(){
+        if(tasksToShow === tasks){
+            handleCompletedFilter();
+        }
+        else{
+            handleShowAllTasks();
+        }
+    }
+
     return(
-        <div>
-
-        </div>
+       <button onClick={toggleCompletedTasks}>{showCompleted ? "Hide Completed Tasks" : "Show Completed Tasks"}</button>
     )
 }
 
