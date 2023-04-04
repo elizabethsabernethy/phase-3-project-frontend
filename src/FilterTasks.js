50359748bfeaaf0f6f9446696efd36ffb0f1ffa3
Elizabeth Abernethy <elizabethsabernethy@gmail.com>
2023-04-04 10:28:51 -0400

    removes everything except base import, function and export

--- src/FilterTasks.js
+++ src/FilterTasks.js
@@ -1,37 +1,13 @@
-import React, { useState } from "react";
+import React from "react";
 
-function FilterTasks({tasks, setTasksToShow, tasksToShow}){
-    const[showCompleted, setShowCompleted] = useState(true)
-
-    const completedTasksToDisplay = tasks.filter((task) => {
-        if (task.completed) {
-        return task;
-        }
-        return false;
-      })
-
-    function handleCompletedFilter(){
-        setTasksToShow(completedTasksToDisplay);
-        setShowCompleted((showCompleted)=>!showCompleted)    
-    }
-
-    function handleShowAllTasks(){
-        setTasksToShow(tasks);
-        setShowCompleted((showCompleted)=>!showCompleted)
-    }
-
-    function toggleCompletedTasks(){
-        if(tasksToShow === tasks){
-            handleCompletedFilter();
-        }
-        else{
-            handleShowAllTasks();
-        }
-    }
+function FilterTasks(){
 
     return(
-       <button onClick={toggleCompletedTasks}>{showCompleted ? "Hide Completed Tasks" : "Show Completed Tasks"}</button>
+        <div>
+            
+        </div>
     )
+
 }
 
 export default FilterTasks;
